281d0c0f7601d5cf3bfa1ad1d0e15f597d600b6e
Luis A. ctv <luis@gmail.com>
2023-08-24 21:14:54 -0400

    segundo 24/8

---
+++ boliviamux/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+
+        <footer class="bg-white">
+            <div class="container mx-auto text-center">
+                © Bolivia Mux, Inc. 2023. We love our users!
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
